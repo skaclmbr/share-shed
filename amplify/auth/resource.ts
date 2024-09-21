71a627cfeab61f8c79fbba65dba54ee1f2a52bd9
Scott Anderson <skaclmbr@gmail.com>
2024-09-21 09:53:30 -0400

    backing up before broken...

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -8,4 +8,31 @@ export const auth = defineAuth({
   loginWith: {
     email: true,
   },
+  userAttributes: {
+    preferredUsername: {
+      mutable: false,
+      required: true
+    },
+    profilePicture: {
+      mutable: true,
+      required: false
+    },
+    phoneNumber: {
+      mutable: true,
+      required: true
+    },
+    givenName: {
+      mutable: true,
+      required: true
+    },
+    familyName: {
+      mutable: true,
+      required: true
+    }
+  },
+  multifactor: {
+    mode: 'OPTIONAL',
+    totp: true
+  },
+  groups: ['ADMINS', 'MANAGERS', 'USERS']
 });
